854e0af60fa55941200455bd6383324bd5d964a0
Christine Xu <christine.x@live.com>
2014-12-24 20:50:10 -0500

    Initial commit

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'facebookAuth' : {
+        'clientID'      : '270319756425199', // your App ID
+        'clientSecret'  : 'bf9804c5e9511a39281368c6d898aaa4', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    }
+};
